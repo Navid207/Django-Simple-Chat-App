31b9a8cf7f31f7807c855294616684ae20fcc9aa
Navid P <Navid.Pazoki@gmx.de>
2024-06-28 11:55:37 +0200

    added messages to the chat html

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, Chat
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text', 'created_at', 'author', 'receiver')
-    list_display = ('author', 'receiver', 'created_at', 'text')
+    fields = ('chat', 'text', 'created_at', 'author', 'receiver')
+    list_display = ('chat', 'author', 'receiver', 'created_at', 'text')
     # search_fields = ('author', 'text',)
 
 # Register your models here.
 
 admin.site.register(Message, MessageAdmin)
-
+admin.site.register(Chat)
--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
 def index(request):
-    return render(request, 'chat/index.html')
+    if request.method == 'POST':
+        print('Received data: '  + request.POST['textmessage'])
+        chat = Chat.objects.get(id=1)
+        Message.objects.create(
+            chat = chat,
+            text = request.POST['textmessage'],
+            author = request.user,
+            receiver = request.user
+        )
+        chatMessages = Message.objects.filter(chat__id = 1)
+    return render(request, 'chat/index.html', {'messages' : chatMessages})
 
