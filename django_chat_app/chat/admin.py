8e9f5f9b02e3b320469f111fc273a26ddb8b63db
Navid P <Navid.Pazoki@gmx.de>
2024-06-28 09:18:19 +0200

    Add new model for the chat

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Message
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'created_at', 'author', 'receiver')
+    list_display = ('author', 'receiver', 'created_at', 'text')
+    # search_fields = ('author', 'text',)
 
 # Register your models here.
+
+admin.site.register(Message, MessageAdmin)
+
