bca5b888547105ac81d5ebbc9d8ed878e9749d25
Navid P <Navid.Pazoki@gmx.de>
2024-07-03 16:14:59 +0200

    added login template

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .models import Chat, Message
+
+@login_required(login_url='/login/')
 
 def index(request):
     if request.method == 'POST':
@@ -11,6 +15,18 @@ def index(request):
             author = request.user,
             receiver = request.user
         )
-        chatMessages = Message.objects.filter(chat__id = 1)
+    chatMessages = Message.objects.filter(chat__id = 1)
     return render(request, 'chat/index.html', {'messages' : chatMessages})
 
+def login_view(request):
+    redirectRout = request.GET.get('next')
+    if request.method == 'POST':
+        print('Received data: '  + request.POST['username'] + request.POST['password'] )
+        user = authenticate(username= request.POST['username'], password= request.POST['password'])
+        if user:
+            login(request, user)
+            return redirect('/chat/')
+        else:
+            return render(request, 'auth/login.html', {'wrongUser': True, 'redirectRout': redirectRout})                
+        
+    return render(request, 'auth/login.html', {'redirectRout': redirectRout})
